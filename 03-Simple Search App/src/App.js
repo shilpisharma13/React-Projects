34b590e79be8d7f927d613156ef166040d5fae07
shilpisharma13 <80937761+shilpisharma13@users.noreply.github.com>
2022-07-03 21:35:07 +0930

    search app done

---
+++ 03-Simple Search App/src/App.js
@@ -0,0 +1,39 @@
+import "./App.css";
+import { useState } from "react";
+import data from "./data";
+
+function App() {
+  const [searchInput, setSearchInput] = useState("");
+
+  return (
+    <section className="container">
+      <form action="">
+        <input
+          type="text"
+          placeholder="Search..."
+          name="search"
+          id="search"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+        {data
+          .filter((value) => {
+            if (
+              value.first_name.toLowerCase().includes(searchInput.toLowerCase())
+            ) {
+              return value;
+            }
+          })
+          .map((value, i) => {
+            return (
+              <div key={i}>
+                <p>{value.first_name}</p>
+              </div>
+            );
+          })}
+      </form>
+    </section>
+  );
+}
+
+export default App;
---
+++ 03-Simple Search App/src/data.js
@@ -0,0 +1,73 @@
+export default
+[
+  {
+    "id": 1,
+    "first_name": "Pietro",
+    "last_name": "Mohammad",
+    "email": "pmohammad0@elegantthemes.com",
+    "ip_address": "123.71.140.193"
+  },
+  {
+    "id": 2,
+    "first_name": "Hailee",
+    "last_name": "Harcarse",
+    "email": "hharcarse1@histats.com",
+    "ip_address": "218.178.142.85"
+  },
+  {
+    "id": 3,
+    "first_name": "Carolina",
+    "last_name": "Odd",
+    "email": "codd2@goo.gl",
+    "ip_address": "96.59.239.52"
+  },
+  {
+    "id": 4,
+    "first_name": "Osborne",
+    "last_name": "Telega",
+    "email": "otelega3@washingtonpost.com",
+    "ip_address": "231.208.69.218"
+  },
+  {
+    "id": 5,
+    "first_name": "Wadsworth",
+    "last_name": "Duguid",
+    "email": "wduguid4@nasa.gov",
+    "ip_address": "146.34.16.24"
+  },
+  {
+    "id": 6,
+    "first_name": "Marissa",
+    "last_name": "Laytham",
+    "email": "mlaytham5@deviantart.com",
+    "ip_address": "250.1.249.88"
+  },
+  {
+    "id": 7,
+    "first_name": "Trudi",
+    "last_name": "Dimmock",
+    "email": "tdimmock6@bluehost.com",
+    "ip_address": "253.92.156.58"
+  },
+  {
+    "id": 8,
+    "first_name": "Dody",
+    "last_name": "Ibbott",
+    "email": "dibbott7@jalbum.net",
+    "ip_address": "61.88.83.5"
+  },
+  {
+    "id": 9,
+    "first_name": "Allys",
+    "last_name": "Kinneir",
+    "email": "akinneir8@flickr.com",
+    "ip_address": "32.79.83.14"
+  },
+  {
+    "id": 10,
+    "first_name": "Dru",
+    "last_name": "Blaylock",
+    "email": "dblaylock9@ibm.com",
+    "ip_address": "239.244.176.20"
+  }
+]
