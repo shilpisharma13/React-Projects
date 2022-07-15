1b90a11083d0c065fd90a27acddeca9bd2253181
shilpisharma13 <shilpi13@gmail.com>
2022-07-15 10:08:13 +0930

    modulus approach added

--- 11-Slider/src/App.js
+++ 11-Slider/src/App.js
@@ -5,28 +5,33 @@ import data from './data'
 function App() {
   const [people, setPeople] = useState(data)
   const [index, setIndex] = React.useState(0)
-  let newIndex
-  const prevSlide = () => {
-    newIndex = index - 1
-    if (index < 0) {
-      newIndex = data.length - 1
-    }
-    return setIndex(newIndex)
-  }
 
-  const nextSlide = () => {
-    newIndex = index + 1
-    if (index > data.length - 1) {
-      newIndex = 0
-    }
-    return setIndex(newIndex)
-  }
+  //Approach using remainder operator but turning the negative values to positive by below modulus fn
+  const mod = (index, length) => ((index % length) + length) % length
+
+  // let newIndex
+  // const prevSlide = () => {
+  //   newIndex = index - 1
+  //   if (index < 0) {
+  //     newIndex = data.length - 1
+  //   }
+  //   return setIndex(newIndex)
+  // }
+
+  // const nextSlide = () => {
+  //   newIndex = index + 1
+  //   if (index > data.length - 1) {
+  //     newIndex = 0
+  //   }
+  //   return setIndex(newIndex)
+  // }
 
   useEffect(() => {
     let slider = setInterval(() => {
-      newIndex = index + 1
-      if (newIndex > data.length - 1) newIndex = 0
-      return setIndex(newIndex)
+      setIndex(mod(index + 1, data.length))
+      // newIndex = index + 1
+      // if (newIndex > data.length - 1) newIndex = 0
+      // return setIndex(newIndex)
     }, 3000)
     return () => {
       clearInterval(slider)
@@ -64,10 +69,16 @@ function App() {
             </article>
           )
         })}
-        <button className='prev' onClick={prevSlide}>
+        <button
+          className='prev'
+          onClick={() => setIndex(mod(index - 1, data.length))}
+        >
           <FiChevronLeft />
         </button>
-        <button className='next' onClick={nextSlide}>
+        <button
+          className='next'
+          onClick={() => setIndex(mod(index + 1, data.length))}
+        >
           <FiChevronRight />
         </button>
       </div>
